7bb4d8d4367e62cdb3ea5b1c3186de6597523112
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-25 19:03:06 -0500

    completed a few tests
     added app test to ensure app fetches data and renders it, and checks if season 1-4 shows up. then test in episodes that each episodes renders

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios'
 
-const fetchShow = () => {
+export const fetchShow = () => {
     return axios
       .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
       .then(res => {
@@ -14,4 +14,3 @@ const fetchShow = () => {
       })
   };
 
-export default fetchShow
